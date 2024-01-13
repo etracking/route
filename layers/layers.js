15cb5081ed136797f5704f52740ce59ae4b1903d
etracking <155722228+etracking@users.noreply.github.com>
2024-01-14 01:12:09 +0800

    Add files via upload

--- layers/layers.js
+++ layers/layers.js
@@ -113,8 +113,8 @@ lyr__6.set('fieldAliases', {'fid': 'fid', 'CP': 'CP', 'Layer': 'Layer', 'Name': 
 lyr_161732024_7.set('fieldAliases', {'fid': 'fid', 'CP': 'CP', 'Layer': 'Layer', 'Name': 'Name', 'Feature': 'Feature', 'Order': 'Order', 'Remark': 'Remark', 'GR': 'GR', });
 lyr_UTM_3.set('fieldImages', {'fid': 'TextEdit', 'id': 'TextEdit', 'left': 'TextEdit', 'top': 'TextEdit', 'right': 'TextEdit', 'bottom': 'TextEdit', 'Zone': 'TextEdit', 'GR': 'TextEdit', 'layer': 'TextEdit', 'path': 'TextEdit', });
 lyr_Route_Q21_22_5.set('fieldImages', {'fid': 'TextEdit', });
-lyr__6.set('fieldImages', {'fid': 'TextEdit', 'CP': 'TextEdit', 'Layer': 'TextEdit', 'Name': 'TextEdit', 'Feature': 'TextEdit', 'Order': 'TextEdit', 'Remark': 'TextEdit', 'GR': 'TextEdit', });
-lyr_161732024_7.set('fieldImages', {'fid': 'TextEdit', 'CP': 'TextEdit', 'Layer': 'TextEdit', 'Name': 'TextEdit', 'Feature': 'TextEdit', 'Order': 'TextEdit', 'Remark': 'TextEdit', 'GR': 'TextEdit', });
+lyr__6.set('fieldImages', {'fid': 'TextEdit', 'CP': 'TextEdit', 'Layer': 'TextEdit', 'Name': 'TextEdit', 'Feature': 'TextEdit', 'Order': 'Range', 'Remark': 'TextEdit', 'GR': 'TextEdit', });
+lyr_161732024_7.set('fieldImages', {'fid': 'TextEdit', 'CP': 'TextEdit', 'Layer': 'TextEdit', 'Name': 'TextEdit', 'Feature': 'TextEdit', 'Order': 'Range', 'Remark': 'TextEdit', 'GR': 'TextEdit', });
 lyr_UTM_3.set('fieldLabels', {'fid': 'no label', 'id': 'no label', 'left': 'no label', 'top': 'no label', 'right': 'no label', 'bottom': 'no label', 'Zone': 'no label', 'GR': 'no label', 'layer': 'no label', 'path': 'no label', });
 lyr_Route_Q21_22_5.set('fieldLabels', {'fid': 'no label', });
 lyr__6.set('fieldLabels', {'fid': 'no label', 'CP': 'no label', 'Layer': 'no label', 'Name': 'no label', 'Feature': 'no label', 'Order': 'no label', 'Remark': 'no label', 'GR': 'no label', });
