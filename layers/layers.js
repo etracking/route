0a3fe7affa879888dadf8b9e2700ead06f63f9c5
etracking <155722228+etracking@users.noreply.github.com>
2024-01-14 01:42:03 +0800

    Add files via upload

--- layers/layers.js
+++ layers/layers.js
@@ -1,57 +1,7 @@
 var wms_layers = [];
 
 
-        var lyr_OSMStandard_0 = new ol.layer.Tile({
-            'title': 'OSM Standard',
-            'type': 'base',
-            'opacity': 1.000000,
-            
-            
-            source: new ol.source.XYZ({
-    attributions: ' &middot; <a href="https://www.openstreetmap.org/copyright">© OpenStreetMap contributors, CC-BY-SA</a>',
-                url: 'http://tile.openstreetmap.org/{z}/{x}/{y}.png'
-            })
-        });
-
-        var lyr_Geodata_1 = new ol.layer.Tile({
-            'title': 'Geodata',
-            'type': 'base',
-            'opacity': 1.000000,
-            
-            
-            source: new ol.source.XYZ({
-    attributions: ' ',
-                url: 'https://mapapi.geodata.gov.hk/gs/api/v1.0.0/xyz/basemap/WGS84/{z}/{x}/{y}.png'
-            })
-        });
-
-        var lyr_Geodata_Label_2 = new ol.layer.Tile({
-            'title': 'Geodata_Label',
-            'type': 'base',
-            'opacity': 1.000000,
-            
-            
-            source: new ol.source.XYZ({
-    attributions: ' ',
-                url: 'https://mapapi.geodata.gov.hk/gs/api/v1.0.0/xyz/label/hk/tc/WGS84/{z}/{x}/{y}.png'
-            })
-        });
-var format_UTM_3 = new ol.format.GeoJSON();
-var features_UTM_3 = format_UTM_3.readFeatures(json_UTM_3, 
-            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_UTM_3 = new ol.source.Vector({
-    attributions: ' ',
-});
-jsonSource_UTM_3.addFeatures(features_UTM_3);
-var lyr_UTM_3 = new ol.layer.Vector({
-                declutter: true,
-                source:jsonSource_UTM_3, 
-                style: style_UTM_3,
-                interactive: true,
-                title: '<img src="styles/legend/UTM_3.png" /> UTM'
-            });
-
-        var lyr_ib20000_4 = new ol.layer.Tile({
+        var lyr_ib20000_0 = new ol.layer.Tile({
             'title': 'ib20000',
             'type': 'base',
             'opacity': 1.000000,
@@ -63,11 +13,5 @@ var lyr_UTM_3 = new ol.layer.Vector({
             })
         });
 
-lyr_OSMStandard_0.setVisible(true);lyr_Geodata_1.setVisible(true);lyr_Geodata_Label_2.setVisible(true);lyr_UTM_3.setVisible(true);lyr_ib20000_4.setVisible(true);
-var layersList = [lyr_OSMStandard_0,lyr_Geodata_1,lyr_Geodata_Label_2,lyr_UTM_3,lyr_ib20000_4];
-lyr_UTM_3.set('fieldAliases', {'fid': 'fid', 'id': 'id', 'left': 'left', 'top': 'top', 'right': 'right', 'bottom': 'bottom', 'Zone': 'Zone', 'GR': 'GR', 'layer': 'layer', 'path': 'path', });
-lyr_UTM_3.set('fieldImages', {'fid': 'TextEdit', 'id': 'TextEdit', 'left': 'TextEdit', 'top': 'TextEdit', 'right': 'TextEdit', 'bottom': 'TextEdit', 'Zone': 'TextEdit', 'GR': 'TextEdit', 'layer': 'TextEdit', 'path': 'TextEdit', });
-lyr_UTM_3.set('fieldLabels', {'fid': 'no label', 'id': 'no label', 'left': 'no label', 'top': 'no label', 'right': 'no label', 'bottom': 'no label', 'Zone': 'no label', 'GR': 'no label', 'layer': 'no label', 'path': 'no label', });
-lyr_UTM_3.on('precompose', function(evt) {
-    evt.context.globalCompositeOperation = 'multiply';
-});
+lyr_ib20000_0.setVisible(true);
+var layersList = [lyr_ib20000_0];
