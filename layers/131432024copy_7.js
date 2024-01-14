333aece1e26aefb555550343d7e3037e15313c83
etracking <155722228+etracking@users.noreply.github.com>
2024-01-14 10:56:04 +0800

    Add files via upload

---
+++ layers/131432024copy_7.js
@@ -0,0 +1,14 @@
+var json_131432024copy_7 = {
+"type": "FeatureCollection",
+"name": "131432024copy_7",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "1", "CP": "SP", "Layer": "O1", "Name": "黃泥頭", "Feature": "黃泥頭巴士站", "Order": 11.0, "Remark": "預留最少60分鐘出發前檢查", "GR": "50QKK 1353 7777" }, "geometry": { "type": "Point", "coordinates": [ 114.217953827918961, 22.382092902264624 ] } },
+{ "type": "Feature", "properties": { "fid": "2", "CP": "B", "Layer": "O1", "Name": "打瀉油坳", "Feature": "五小徑交匯", "Order": 13.0, "Remark": null, "GR": "50QKK 1554 7679" }, "geometry": { "type": "Point", "coordinates": [ 114.237631109948467, 22.373584210477386 ] } },
+{ "type": "Feature", "properties": { "fid": "3", "CP": "CS", "Layer": "O1", "Name": "白沙灣", "Feature": "營地扶輪樓", "Order": 15.0, "Remark": "營地", "GR": "50QKK 1786 7593" }, "geometry": { "type": "Point", "coordinates": [ 114.260292590292053, 22.366206689886162 ] } },
+{ "type": "Feature", "properties": { "fid": "4", "CP": "E", "Layer": "O1", "Name": "匯流涼亭", "Feature": "六小徑交匯", "Order": 17.0, "Remark": null, "GR": "50QKK 1675 7924" }, "geometry": { "type": "Point", "coordinates": [ 114.248935925539456, 22.395892333375823 ] } },
+{ "type": "Feature", "properties": { "fid": "5", "CP": "G", "Layer": "O1", "Name": "鹿巢坳", "Feature": "小徑交匯", "Order": 19.0, "Remark": null, "GR": "50QKK 1483 7981" }, "geometry": { "type": "Point", "coordinates": [ 114.230202620401144, 22.400717470682256 ] } },
+{ "type": "Feature", "properties": { "fid": "6", "CP": "EP", "Layer": "O1", "Name": "黃泥頭", "Feature": "黃泥頭巴士站", "Order": 22.0, "Remark": "終點", "GR": "50QKK 1353 7777" }, "geometry": { "type": "Point", "coordinates": [ 114.217953827918961, 22.382092902264624 ] } },
+{ "type": "Feature", "properties": { "fid": "7", "CP": "H", "Layer": "O1", "Name": "梅子林", "Feature": "限制道路公路與小徑交匯", "Order": 20.0, "Remark": null, "GR": "50QKK 1513 7895" }, "geometry": { "type": "Point", "coordinates": [ 114.233267522135662, 22.39300710590333 ] } }
+]
+}
