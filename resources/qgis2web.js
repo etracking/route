a9c69f47dc06f0b819d2abd40b8b924313e17ff7
etracking <155722228+etracking@users.noreply.github.com>
2024-01-14 09:58:50 +0800

    Add files via upload

--- resources/qgis2web.js
+++ resources/qgis2web.js
@@ -31,13 +31,8 @@ var map = new ol.Map({
     })
 });
 
-var layerSwitcher = new ol.control.LayerSwitcher({tipLabel: "Layers"});
-map.addControl(layerSwitcher);
-layerSwitcher.hidePanel = function() {};
-layerSwitcher.showPanel();
 
-
-map.getView().fit([12709971.323045, 2554854.661539, 12723141.487002, 2560445.931091], map.getSize());
+map.getView().fit([12707884.027822, 2554874.361672, 12724377.333246, 2560466.650114], map.getSize());
 
 var NO_POPUP = 0
 var ALL_FIELDS = 1
