61bdcdf8e9517b56d0f97fbf9165de64d2f83580
etracking <155722228+etracking@users.noreply.github.com>
2024-01-31 19:55:24 +0800

    Add files via upload

--- layers/422024_8.js
+++ layers/422024_8.js
@@ -6,8 +6,8 @@ var json_422024_8 = {
 { "type": "Feature", "properties": { "CP": "SP", "Name": null, "Feature": "已建區", "Remark": null, "GR": "50QJK 9487 8495" }, "geometry": { "type": "Point", "coordinates": [ 114.035582991288123, 22.443627689467185 ] } },
 { "type": "Feature", "properties": { "CP": "CP1", "Name": null, "Feature": "公路交匯", "Remark": null, "GR": "50QJK 9566 8495" }, "geometry": { "type": "Point", "coordinates": [ 114.043253749397252, 22.443792954677367 ] } },
 { "type": "Feature", "properties": { "CP": "CP2", "Name": null, "Feature": "公路小徑交匯", "Remark": null, "GR": "50QJK 9612 8412" }, "geometry": { "type": "Point", "coordinates": [ 114.04788884739996, 22.436330329131174 ] } },
-{ "type": "Feature", "properties": { "CP": "CP3", "Name": null, "Feature": "三角網測站220", "Remark": null, "GR": "50QJK 9633 8346" }, "geometry": { "type": "Point", "coordinates": [ 114.050045202775635, 22.430435713467872 ] } },
-{ "type": "Feature", "properties": { "CP": "CP4", "Name": null, "Feature": "公路及四小徑交匯", "Remark": null, "GR": "50QJK 9685 8324" }, "geometry": { "type": "Point", "coordinates": [ 114.055164220859297, 22.428560313087111 ] } },
-{ "type": "Feature", "properties": { "CP": "EP", "Name": null, "Feature": "鐵路站", "Remark": null, "GR": "50QJK 9774 8383" }, "geometry": { "type": "Point", "coordinates": [ 114.063662621834794, 22.434026208305916 ] } }
+{ "type": "Feature", "properties": { "CP": "CP3", "Name": null, "Feature": "三角網測站220m", "Remark": null, "GR": "50QJK 9633 8346" }, "geometry": { "type": "Point", "coordinates": [ 114.050045202775635, 22.430435713467872 ] } },
+{ "type": "Feature", "properties": { "CP": "CP4", "Name": null, "Feature": "公路小徑交匯", "Remark": null, "GR": "50QJK 9685 8323" }, "geometry": { "type": "Point", "coordinates": [ 114.055131145046118, 22.428433680914701 ] } },
+{ "type": "Feature", "properties": { "CP": "EP", "Name": null, "Feature": "鐵路站", "Remark": null, "GR": "50QJK 9765 8398" }, "geometry": { "type": "Point", "coordinates": [ 114.062708439042922, 22.435396050028213 ] } }
 ]
 }
