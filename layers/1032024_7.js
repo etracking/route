599490675c40975e5876c6e54fb8b6657e62746a
etracking <155722228+etracking@users.noreply.github.com>
2024-02-20 01:42:29 +0800

    Add files via upload

--- layers/1032024_7.js
+++ layers/1032024_7.js
@@ -3,7 +3,6 @@ var json_1032024_7 = {
 "name": "1032024_7",
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
 "features": [
-{ "type": "Feature", "properties": { "CP": "EP", "Name": "紅梅谷燒烤場", "Feature": "已建區旁", "Remark": null, "GR": "50QKK 0970 7583" }, "geometry": { "type": "Point", "coordinates": [ 114.181145937060919, 22.363944205026538 ] } },
 { "type": "Feature", "properties": { "CP": "SP", "Name": "沙田童軍中心", "Feature": "已建區", "Remark": null, "GR": "50QKK 1122 7654" }, "geometry": { "type": "Point", "coordinates": [ 114.195763376281533, 22.370607021595372 ] } },
 { "type": "Feature", "properties": { "CP": "SP/EP", "Name": "作壆坑村小巴站", "Feature": null, "Remark": null, "GR": "50QKK 1119 7666" }, "geometry": { "type": "Point", "coordinates": [ 114.195450617957903, 22.371684768698721 ] } },
 { "type": "Feature", "properties": { "CP": "SP", "Name": "水泉澳邨商場門口", "Feature": null, "Remark": null, "GR": "50QKK 1139 7687" }, "geometry": { "type": "Point", "coordinates": [ 114.197353013786596, 22.373613298125157 ] } },
