cc559b57bb6b79c4e49d06cb480fb1a0d4b74272
etracking <155722228+etracking@users.noreply.github.com>
2024-04-06 12:07:28 +0800

    Add files via upload

---
+++ layers/422024_11.js
@@ -0,0 +1,13 @@
+var json_422024_11 = {
+"type": "FeatureCollection",
+"name": "422024_11",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "CP": "SP", "Name": null, "Feature": "已建區", "Remark": null, "GR": "50QJK 9487 8495" }, "geometry": { "type": "Point", "coordinates": [ 114.035582991288123, 22.443627689467185 ] } },
+{ "type": "Feature", "properties": { "CP": "CP1", "Name": null, "Feature": "公路交匯", "Remark": null, "GR": "50QJK 9566 8495" }, "geometry": { "type": "Point", "coordinates": [ 114.043253749397252, 22.443792954677367 ] } },
+{ "type": "Feature", "properties": { "CP": "CP2", "Name": null, "Feature": "公路小徑交匯", "Remark": null, "GR": "50QJK 9612 8412" }, "geometry": { "type": "Point", "coordinates": [ 114.04788884739996, 22.436330329131174 ] } },
+{ "type": "Feature", "properties": { "CP": "CP3", "Name": null, "Feature": "三角網測站220m", "Remark": null, "GR": "50QJK 9633 8346" }, "geometry": { "type": "Point", "coordinates": [ 114.050045202775635, 22.430435713467872 ] } },
+{ "type": "Feature", "properties": { "CP": "CP4", "Name": null, "Feature": "公路小徑交匯", "Remark": null, "GR": "50QJK 9685 8323" }, "geometry": { "type": "Point", "coordinates": [ 114.055131145046118, 22.428433680914701 ] } },
+{ "type": "Feature", "properties": { "CP": "EP", "Name": null, "Feature": "鐵路站", "Remark": null, "GR": "50QJK 9765 8398" }, "geometry": { "type": "Point", "coordinates": [ 114.062708439042922, 22.435396050028213 ] } }
+]
+}
---
+++ layers/StaffReference_14.js
@@ -0,0 +1,24 @@
+var json_StaffReference_14 = {
+"type": "FeatureCollection",
+"name": "StaffReference_14",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "CP": "H", "Name": "梅子林", "Feature": "限制道路公路與小徑交匯", "Remark": null, "GR": "50QKK 1513 7895" }, "geometry": { "type": "Point", "coordinates": [ 114.233267522135662, 22.39300710590333 ] } },
+{ "type": "Feature", "properties": { "CP": "A", "Name": "石芽背", "Feature": "四小徑交匯", "Remark": null, "GR": "50QKK 1435 7651" }, "geometry": { "type": "Point", "coordinates": [ 114.226135571497011, 22.370859996349225 ] } },
+{ "type": "Feature", "properties": { "CP": "C", "Name": "三塊田", "Feature": "公路交匯", "Remark": null, "GR": "50QKK 1695 7624" }, "geometry": { "type": "Point", "coordinates": [ 114.251408910717615, 22.368854270758526 ] } },
+{ "type": "Feature", "properties": { "CP": "D", "Name": "屋場", "Feature": "限制道路入口", "Remark": null, "GR": "50QKK 1717 7743" }, "geometry": { "type": "Point", "coordinates": [ 114.253332519629666, 22.379628759772277 ] } },
+{ "type": "Feature", "properties": { "CP": "F", "Name": "鹿巢山", "Feature": "鹿巢山", "Remark": null, "GR": "50QKK 1594 7952" }, "geometry": { "type": "Point", "coordinates": [ 114.241025897729997, 22.398284933925339 ] } },
+{ "type": "Feature", "properties": { "CP": "J", "Name": "女婆坳", "Feature": "四小徑交匯", "Remark": null, "GR": "50QKK 1458 7843" }, "geometry": { "type": "Point", "coordinates": [ 114.228023628952641, 22.388223421913249 ] } },
+{ "type": "Feature", "properties": { "CP": "TP1", "Name": null, "Feature": null, "Remark": null, "GR": "50QKK 1151 6687" }, "geometry": { "type": "Point", "coordinates": [ 114.200319786291246, 22.283399864795836 ] } },
+{ "type": "Feature", "properties": { "CP": "TP2", "Name": null, "Feature": null, "Remark": null, "GR": "50QKK 1134 6633" }, "geometry": { "type": "Point", "coordinates": [ 114.198768645449988, 22.278498770726493 ] } },
+{ "type": "Feature", "properties": { "CP": "TP3", "Name": null, "Feature": null, "Remark": null, "GR": "50QKK 1123 6632" }, "geometry": { "type": "Point", "coordinates": [ 114.197703953701847, 22.278390121022468 ] } },
+{ "type": "Feature", "properties": { "CP": "TP4", "Name": null, "Feature": null, "Remark": null, "GR": "50QKK 1179 6607" }, "geometry": { "type": "Point", "coordinates": [ 114.203178259277848, 22.276227938747802 ] } },
+{ "type": "Feature", "properties": { "CP": "TP01", "Name": null, "Feature": "公路", "Remark": null, "GR": "50QJK 9591 8432" }, "geometry": { "type": "Point", "coordinates": [ 114.045756755477939, 22.438155239689443 ] } },
+{ "type": "Feature", "properties": { "CP": "TP02", "Name": null, "Feature": "3小徑交匯", "Remark": null, "GR": "50QJK 9627 8417" }, "geometry": { "type": "Point", "coordinates": [ 114.049279246778809, 22.436865872552264 ] } },
+{ "type": "Feature", "properties": { "CP": "TP03", "Name": null, "Feature": "小丘頂", "Remark": null, "GR": "50QJK 9634 8408" }, "geometry": { "type": "Point", "coordinates": [ 114.049975780954625, 22.436066310952629 ] } },
+{ "type": "Feature", "properties": { "CP": "TP04", "Name": null, "Feature": "山坳", "Remark": null, "GR": "50QJK 9634 8398" }, "geometry": { "type": "Point", "coordinates": [ 114.049994868302804, 22.435164105582214 ] } },
+{ "type": "Feature", "properties": { "CP": "TP05", "Name": null, "Feature": "3點測量", "Remark": null, "GR": "50QJK 9624 8358" }, "geometry": { "type": "Point", "coordinates": [ 114.0491007382536, 22.43153753938952 ] } },
+{ "type": "Feature", "properties": { "CP": "TP06", "Name": null, "Feature": "小丘頂", "Remark": null, "GR": "50QJK 9651 8345" }, "geometry": { "type": "Point", "coordinates": [ 114.051745801870254, 22.430412564117809 ] } },
+{ "type": "Feature", "properties": { "CP": "TP07", "Name": null, "Feature": "3點測量", "Remark": null, "GR": "50QJK 9669 8351" }, "geometry": { "type": "Point", "coordinates": [ 114.053481202063764, 22.430985793459154 ] } }
+]
+}
