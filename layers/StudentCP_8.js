7a261e131548b3579b23a6959539922b84074641
etracking <155722228+etracking@users.noreply.github.com>
2024-04-08 08:52:54 +0800

    Add files via upload

--- layers/StudentCP_8.js
+++ layers/StudentCP_8.js
@@ -4,6 +4,7 @@ var json_StudentCP_8 = {
 "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
 "features": [
 { "type": "Feature", "properties": { "CP": "T1(1)", "Name": null, "Feature": null, "Remark": null, "T1": "T1:1039/1049", "T2": null, "T3": null, "T4": null, "GR": "49QHE 0768 8091" }, "geometry": { "type": "Point", "coordinates": [ 113.988425034114684, 22.406749322366935 ] } },
+{ "type": "Feature", "properties": { "CP": "T1(3)", "Name": null, "Feature": null, "Remark": null, "T1": "T1:1023/1033", "T2": null, "T3": null, "T4": null, "GR": "50QJK 9600 7960" }, "geometry": { "type": "Point", "coordinates": [ 114.04753119093489, 22.395587151500301 ] } },
 { "type": "Feature", "properties": { "CP": "T1(5)", "Name": null, "Feature": null, "Remark": null, "T1": "T1:1418/1428", "T2": null, "T3": null, "T4": null, "GR": "50QKK 0002 7766" }, "geometry": { "type": "Point", "coordinates": [ 114.086899563463987, 22.378791697888012 ] } }
 ]
 }
